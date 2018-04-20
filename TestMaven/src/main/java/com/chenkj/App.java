b749ed8a8ac0ef83a9bca45c8d073964c30ce671
chenkj <1097823199@qq.com>
2018-04-20 16:53:50 +0800

    add TestMaven

---
+++ TestMaven/src/main/java/com/chenkj/App.java
@@ -0,0 +1,13 @@
+package com.chenkj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
